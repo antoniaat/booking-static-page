4bb021f3274b72dc34eb320dc837a64e6a86069b
Antonia Atanasova <antoniyaata@gmail.com>
2021-09-22 19:12:49 +0300

    Added booking bar component structure and styles

--- booking-page/src/App.tsx
+++ booking-page/src/App.tsx
@@ -2,11 +2,13 @@ import './styles/index.scss';
 
 import React from 'react';
 import { Navigation } from './components/navigation/navigation.components';
+import { BookingBar } from './components/booking-bar/booking-bar.component';
 
 const App = () => {
   return (
     <div className="App">
       <Navigation />
+      <BookingBar />
     </div>
   );
 }
---
+++ booking-page/src/components/booking-bar/booking-bar.component.tsx
@@ -0,0 +1,66 @@
+import 'react-dropdown/style.css';
+import './booking-bar.styles.scss';
+
+import React from 'react';
+import Dropdown from 'react-dropdown';
+import { FaBars, FaSearch } from 'react-icons/fa';
+
+const options = [
+    'one', 'two', 'three'
+];
+
+const fromToData = [
+    '3 Jan 2020 - 28 Feb 2020',
+    '4 Feb 2020 - 5 March 2020',
+    '12 Sep 2020 - 15 Sep 2020',
+    '29 Nov 2020 - 30 Nov 2020',
+]
+
+const forData = [
+    '2 Guests',
+    '3 Guests',
+    '4 Guests',
+]
+
+
+export const BookingBar = () => {
+    return (
+        <nav className="booking-bar">
+            <ul className="booking-bar-list">
+                <li className="booking-bar-list-item">
+                    <p className="booking-bar-list-item-label">
+                        From / To
+                    </p>
+                    <Dropdown className="booking-bar-list-item-dropdown" options={fromToData} value={fromToData[0]} />
+                </li>
+                <li className="booking-bar-list-item">
+                    <p className="booking-bar-list-item-label">
+                        For
+                    </p>
+                    <Dropdown className="booking-bar-list-item-dropdown" options={forData} value={forData[0]} />
+                </li>
+                <li className="booking-bar-list-item">
+                    <p className="booking-bar-list-item-label">
+                        £ Per night
+                    </p>
+                    <p className="booking-bar-list-item-value">
+                        345
+                    </p>
+                </li>
+                <li className="booking-bar-list-item">
+                    <p className="booking-bar-list-item-label">
+                        £ Total for 54 nights
+                    </p>
+                    <p className="booking-bar-list-item-value">
+                        18, 630
+                    </p>
+                </li>
+                <li className="booking-bar-list-item">
+                    <button className="btn btn-primary">
+                        INSTANT BOOKING
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    )
+}
